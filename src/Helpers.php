4d0dc670e2f359d14b3382147c28ff196dec4efb
ndnam90 <ndnam90@gmail.com>
2017-09-05 23:27:52 +0700

    add set function to helpers

--- src/Helpers.php
+++ src/Helpers.php
@@ -6,8 +6,12 @@ if( ! function_exists ('setting')) {
 
         $setting = new Potato\Settings\Setting();
 
-        if(func_num_args()) {
+        if(func_num_args() == 1) {
             return $setting->get(func_get_arg(0));
+        }
+
+        if(func_num_args() == 2) {
+            return $setting->set(func_get_arg(0), func_get_arg(1));
         }
 
         return $setting;
